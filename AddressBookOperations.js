5a6908fa555fe65c9bc2243efd0a2074e233621f
SaiVenkat12 <gsaiv1001@gmail.com>
2023-03-31 22:09:53 +0530

    [Sai Venkat]- Added-Delete contacts in js

--- AddressBookOperations.js
+++ AddressBookOperations.js
@@ -3,13 +3,31 @@ const utility = require('./AddressBookUtility');
 const AddressBookUtility = require('./AddressBookUtility');
 
 function Operations() {
-    const select = readlineSync.question('Enter the choice A)Add Contact - ')
-        switch (select) {
-            case 'A':
+    const select = readlineSync.question('Enter the choice A)Add Contact B)Add Multiple Contacts D)Delete Last Contact d)Display \nEnter any other to Exit: ')
+    switch (select) {
+        case 'A':
+            utility.AddContacts();
+            Operations();
+            break;
+        case 'B':
+            let n = readlineSync.question('Enter the Number of Contacts Needed: ');
+            for (i = 1; i <= n; i++) {
                 utility.AddContacts();
-                utility.Display();
-                break;
-        }
-    
+            }
+            Operations();
+            break;
+        case 'D':
+            utility.DeleteContact();
+            Operations();
+            break;
+        case 'd':
+            utility.Display();
+            Operations();
+            break;
+        default:
+            console.log("Exit");
+            break;
+    }
+
 }
 Operations();
--- AddressBookUtility.js
+++ AddressBookUtility.js
@@ -10,20 +10,34 @@ class Utility {
         let state = readlineSync.question('Enter the state: ');
         let city = readlineSync.question('Enter the city: ');
         let zip = readlineSync.question('Enter the ZipCode: ');
+
         let newContact = {
             firstName: firstName,
             lastName: lastName,
             email: email,
             phNo: phNo,
             state: state,
-            city:city,
-            zip:zip,
+            city: city,
+            zip: zip,
         }
         addressBookArray.push(newContact);
+        console.log("Contact Added!");
     }
-
-    Display(){
-        console.log(addressBookArray);
+    DeleteContact() {
+        let name = readlineSync.question('Enter the First Name of the person to delete:');
+        let index = addressBookArray.findIndex(x => x.firstName === name);
+        if (index !== -1) {
+            addressBookArray.splice(index, 1);
+            console.log('Contact Deleted!');
+        } else {
+            console.log('No Contact found with Name: ', name);
+        }
+    }
+    Display() {
+        addressBookArray.forEach(function(data){
+        console.log("Name: " + data.firstName + " " + data.lastName + "\n EMAIL:  " + data.email + "\n PhoneNumber: " + data.phNo + "\n City: " + data.city + "\n State: " + data.state + " Zipcode: " + data.zip)
+        });
     }
 }
+
 module.exports = new Utility();
