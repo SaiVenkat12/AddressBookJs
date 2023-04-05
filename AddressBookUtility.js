7469f52e3b5c8b782878382be434ca688f17f7b3
SaiVenkat12 <gsaiv1001@gmail.com>
2023-04-05 08:41:33 +0530

    [Sai Venkat]- Added-Checking contacts for duplictes in js

--- AddressBookUtility.js
+++ AddressBookUtility.js
@@ -2,73 +2,78 @@ var readlineSync = require('readline-sync');
 const fs = require('fs');
 let addressBookArray = new Array();
 
-const nameValidation=RegExp("^[A-Z]{1}[a-z]{2,}$");
-const phValidation=RegExp("^[0-9]{2}[6-9]{1}[0-9]{9}$");
-const emailValidation=RegExp("^[a-zA-Z0-9]([.+-_]?[a-zA-z0-9])*@[0-9a-zA-Z]+.[a-zA-Z]{2,}([.]{0,1}[A-Za-z]{2,})$");
-const zipValidation=RegExp("^[0-9]{6}$");
+const nameValidation = RegExp("^[A-Z]{1}[a-z]{2,}$");
+const phValidation = RegExp("^[0-9]{2}[6-9]{1}[0-9]{9}$");
+const emailValidation = RegExp("^[a-zA-Z0-9]([.+-_]?[a-zA-z0-9])*@[0-9a-zA-Z]+.[a-zA-Z]{2,}([.]{0,1}[A-Za-z]{2,})$");
+const zipValidation = RegExp("^[0-9]{6}$");
 
 class Utility {
     AddContacts() {
-        
+
         let firstName = readlineSync.question('Enter the First Name: ');
-        let flag=true;
-        while(flag){
-            if(!nameValidation.test(firstName)){
+        let flag = true;
+        while (flag) {
+            if (!nameValidation.test(firstName)) {
                 console.log("---Name is Invalid!!!---");
                 firstName = readlineSync.question('Enter the Last Name: ');
             }
-            else{
-                flag=false;
+            else {
+                flag = false;
             }
         }
 
         let lastName = readlineSync.question('Enter the Last Name: ');
-        flag=true;
-        while(flag){
-            if(!nameValidation.test(lastName)){
+        flag = true;
+        while (flag) {
+            if (!nameValidation.test(lastName)) {
                 console.log("---Name is Invalid!!!---");
                 lastName = readlineSync.question('Enter the Last Name: ');
             }
-            else{
-                flag=false;
+            else {
+                flag = false;
             }
         }
-
+        let index = addressBookArray.findIndex(x => x.firstName === firstName && x.lastName === lastName);
+            if (index != -1) {
+                console.log("----Contact already exists with this name-----");
+                return this.AddContacts()
+            }
+            
         let email = readlineSync.question('Enter the Email: ');
-        flag=true;
-        while(flag){
-            if(!emailValidation.test(email)){
+        flag = true;
+        while (flag) {
+            if (!emailValidation.test(email)) {
                 console.log("---email is Invalid!!!---");
                 email = readlineSync.question('Enter the Email: ');
             }
-            else{
-                flag=false;
+            else {
+                flag = false;
             }
         }
 
         let phNo = readlineSync.question('Enter the Phone Number: ');
-        flag=true;
-        while(flag){
-            if(!phValidation.test(phNo)){
+        flag = true;
+        while (flag) {
+            if (!phValidation.test(phNo)) {
                 console.log("---Phone Number is Invalid!!!---");
                 phNo = readlineSync.question('Enter the Phone Number:  ');
             }
-            else{
-                flag=false;
+            else {
+                flag = false;
             }
         }
 
         let state = readlineSync.question('Enter the state: ');
         let city = readlineSync.question('Enter the city: ');
         let zip = readlineSync.question('Enter the ZipCode: ');
-        flag=true;
-        while(flag){
-            if(!zipValidation.test(zip)){
+        flag = true;
+        while (flag) {
+            if (!zipValidation.test(zip)) {
                 console.log("---Zip Code is Invalid!!!---");
                 zip = readlineSync.question('Enter the ZipCode: ');
             }
-            else{
-                flag=false;
+            else {
+                flag = false;
             }
         }
 
@@ -83,10 +88,9 @@ class Utility {
         }
         addressBookArray.push(newContact);
         let json = JSON.stringify(addressBookArray);
-        fs.writeFileSync('addressBookFile.json',json);
+        fs.writeFileSync('addressBookFile.json', json);
         console.log("Contact Added!");
     }
-    
     DeleteContact() {
         let name = readlineSync.question('Enter the First Name of the person to delete:');
         let index = addressBookArray.findIndex(x => x.firstName === name);
@@ -94,7 +98,7 @@ class Utility {
             addressBookArray.splice(index, 1);
             console.log('Contact Deleted!');
             let json = JSON.stringify(addressBookArray);
-            fs.writeFileSync('addressBookFile.json',json);
+            fs.writeFileSync('addressBookFile.json', json);
         } else {
             console.log('No Contact found with Name: ', name);
         }
@@ -104,18 +108,18 @@ class Utility {
         let index = addressBookArray.findIndex(x => x.firstName === name);
         if (index !== -1) {
             console.log("Contact Found");
-            console.log("Name: "+addressBookArray[index].firstName+" "+addressBookArray[index].lastName+"\nPhoneNumber: "+addressBookArray[index].phNo+"\nEMail:"+addressBookArray[index].email);
+            console.log("Name: " + addressBookArray[index].firstName + " " + addressBookArray[index].lastName + "\nPhoneNumber: " + addressBookArray[index].phNo + "\nEMail:" + addressBookArray[index].email);
         }
         else
             console.log("Contact not Found");
     }
-    Filter(){
+    Filter() {
         let city = readlineSync.question('Enter the city: ');
-        let cityArr=addressBookArray.filter(x=>x.city===city);
-        if(cityArr!=null){
+        let cityArr = addressBookArray.filter(x => x.city === city);
+        if (cityArr != null) {
             console.log(cityArr);
         }
-        else{
+        else {
             console.log("No Contact found");
         }
     }
@@ -156,7 +160,7 @@ class Utility {
             }
             console.log("Contact Updated");
             let json = JSON.stringify(addressBookArray);
-            fs.writeFileSync('addressBookFile.json',json);
+            fs.writeFileSync('addressBookFile.json', json);
         } else {
             console.log('No Contact found with Name: ', name);
         }
